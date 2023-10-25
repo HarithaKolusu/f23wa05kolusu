1d856460069cdc09dfb4ad6f76de230267177933
s564205 <s564205@nwmissouri.edu>
2023-10-24 22:12:56 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Haritha Kolusu' });
+  res.render('mydata', { title: 'Haritha Kolusu'});
 });
 
 module.exports = router;
